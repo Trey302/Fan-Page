032e4204f9be0496360226d74b6af696773e1432
Route9Library <you@example.com>
2020-10-29 18:29:46 -0400

    more

--- function.js
+++ function.js
@@ -1,12 +1,4 @@
 
-const hamburger_menu = document.querySelector(".hamburger-menu");
-const container = document.querySelector(".container");
-
-hamburger_menu.addEventListener("click", () => {
-    container.classList.toggle("active");
-});
-
-
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
